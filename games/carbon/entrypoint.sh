07e00a0ed4146e71ad603441bdebbb1f9a154ac9
SturdyStubs <mattl321@hotmail.com>
2024-03-29 01:56:38 +0100

    another fix

--- games/carbon/entrypoint.sh
+++ games/carbon/entrypoint.sh
@@ -28,7 +28,7 @@ if [[ "${FRAMEWORK}" != "oxide" ]]; then
 fi
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 if [[ "$OXIDE" == "1" ]] || [[ "${FRAMEWORK}" == "oxide" ]]; then
