e9effebd1b822cb3f2313c4c5b0aa491465ec3b5
SturdyStubs <mattl321@hotmail.com>
2024-03-29 03:03:50 +0100

    revert+

--- games/carbon/entrypoint.sh
+++ games/carbon/entrypoint.sh
@@ -5,8 +5,8 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $(NF-2);exit}'`
 
 
-## if auto_update is not set or to 1 update
-if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" = 1 ]; then
+# if auto_update is not set or to 1 update
+ if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then
 	./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 258550 validate +quit
 else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
