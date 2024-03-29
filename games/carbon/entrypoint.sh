69b0ce02aa499400c9bdb7c18aaf3d6769648383
BippyMiester <admin@jdswebservice.com>
2024-03-29 02:15:03 -0400

    Fixed Syntax Issues

--- games/carbon/entrypoint.sh
+++ games/carbon/entrypoint.sh
@@ -17,7 +17,7 @@ fi
 # Do the same for oxide
 if [[ "${FRAMEWORK}" =~ "oxide" ]]; then
     echo "Checking MODDING_ROOT folder compatibility"
-    if [[ "${MODDING_ROOT}" =~ "carbon" ]] || [[ ! "${MODDING_ROOT}" =~ "oxide" ]];; then
+    if [[ "${MODDING_ROOT}" =~ "carbon" ]] || [[ ! "${MODDING_ROOT}" =~ "oxide" ]]; then
         echo "Your framework is ${FRAMEWORK} but your MODDING_ROOT folder does not contain the word \"oxide\". Please change the MODDING_ROOT variable to contain the word \"oxide\" for compatibility reasons."
         exit 0
     fi
