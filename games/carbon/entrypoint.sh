1df0c7e735798fb5ec009470d9ee7975bbb74076
BippyMiester <admin@jdswebservice.com>
2024-07-31 12:15:40 -0400

    Force Validate Files on Carbon Switch from Oxide

--- games/carbon/entrypoint.sh
+++ games/carbon/entrypoint.sh
@@ -79,12 +79,14 @@ printf "${GREEN}Compatibility check passed...${NC}"
 
 # Clean Up Files from Oxide to Vanilla/Carbon Switch
 if [[ "${FRAMEWORK}" != "oxide" ]] || [[ "${FRAMEWORK}" != "oxide-staging" ]]; then
+    CARBONSWITCH="FALSE"
     printf "${BLUE}Modding framework is not set to Oxide. Checking if there are left over Oxide files in the server.${NC}"
     shopt -s nullglob
     # Check if the Oxide.dll files exist
     files=(/home/container/RustDedicated_Data/Managed/Oxide.*.dll)
     if [ ${#files[@]} -gt 0 ]; then
         echo "Oxide Files Found!"
+        CARBONSWITCH="TRUE"
         if [[ "${FRAMEWORK}" =~ "carbon" ]]; then
             # Check to see if any Oxide extensions need to be moved
             echo "Carbon framework detected! Moving Oxide Extentions if the exist."
@@ -217,6 +219,10 @@ if [ -z "${AUTO_UPDATE}" ] || [ "${AUTO_UPDATE}" == "1" ]; then
             ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 258550 +quit
         fi
     fi
+elif [[ "${CARBONSWITCH}" == "TRUE" ]]; then
+    echo -e "Carbon Switch Detected! Validating Game Files!"
+    echo -e "Updating game server..."
+    ./steamcmd/steamcmd.sh +force_install_dir /home/container +login anonymous +app_update 258550 validate +quit
 else
     printf "${YELLOW} Not updating server, auto update set to false.${NC}"
 fi
