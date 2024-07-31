8a336b9d94dfcc2aa78b3c107fc376e4036a3f38
BippyMiester <admin@jdswebservice.com>
2024-07-31 17:47:02 -0400

    Added Comment

--- games/carbon/entrypoint.sh
+++ games/carbon/entrypoint.sh
@@ -186,6 +186,7 @@ sleep 10
 
 # Replace Startup Variables (Keep this here. Important. Forgot exactly what the command does. But here's GPT's interpretation of it)
 # https://capture.dropbox.com/amLrR7iuKdJ3kSY6
+# Takes the start up command and converts the {{}} into the appropriate bash syntax?
 MODIFIED_STARTUP=$(eval echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
