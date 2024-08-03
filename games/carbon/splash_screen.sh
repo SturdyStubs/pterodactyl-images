2c19b149b569107e7b50facc6bcd20439f104b09
BippyMiester <admin@jdswebservice.com>
2024-08-03 12:08:35 -0400

    Updated Splash Screen

--- games/carbon/splash_screen.sh
+++ games/carbon/splash_screen.sh
@@ -1,15 +1,13 @@
 source /helpers/colors.sh
 
-printf "${RED}"
-printf "╭──────────────────────────────────────────────────╮\n"
-printf "│                 AIO RUST EGG                     │\n"
-printf "│            Created By: SturdyStubs               │\n"
-printf "├──────────────────────────────────────────────────┤\n"
-printf "│    For More Information See The Documentation    │\n"
-printf "│        https://tinyurl.com/aiorustegg            │\n"
-printf "│                 Version 2.0.0                    │\n"
-printf "╰──────────────────────────────────────────────────╯\n"
-printf "${NC}"
+printf "${RED}╭──────────────────────────────────────────────────╮${NC}"
+printf "${RED}│                 AIO RUST EGG                     │${NC}"
+printf "${RED}│            Created By: SturdyStubs               │${NC}"
+printf "${RED}├──────────────────────────────────────────────────┤${NC}"
+printf "${RED}│    For More Information See The Documentation    │${NC}"
+printf "${RED}│        https://tinyurl.com/aiorustegg            │${NC}"
+printf "${RED}│                 Version 2.0.0                    │${NC}"
+printf "${RED}╰──────────────────────────────────────────────────╯${NC}"
 
 printf "${BLUE}Starting Egg Now!${NC}"
 sleep 2
