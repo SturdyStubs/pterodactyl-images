6e02ac3154faf0c5aa3f50b89199c035d1b57f51
SturdyStubs <mattl321@hotmail.com>
2024-08-14 14:02:35 +0200

    fix for LOG_FILE detection

--- games/carbon/sections/log_file.sh
+++ games/carbon/sections/log_file.sh
@@ -9,7 +9,7 @@ Debug "Inside /sections/log_file.sh file!"
 # LOG FILE CHECK #
 ##################
 
-if [ -z "${LOG_FILE}" ] || { [ "${LOG_FILE}" != "0" ] || [ "${LOG_FILE}" != "1" ]; }; then
+if [ -z "${LOG_FILE}" ] || { [ "${LOG_FILE}" != "0" ] && [ "${LOG_FILE}" != "1" ]; }; then
     Warn "LOG_FILE variable not found. Update your egg at https://github.com/SturdyStubs/AIO.Egg. Disabling file logging..."
     LOG_FILE="0"
 fi
