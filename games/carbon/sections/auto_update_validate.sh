8bab25e84f1e636224911eceb5dcc108151b4228
SturdyStubs <mattl321@hotmail.com>
2024-08-14 13:23:58 +0200

    chmod DepotDownloader

--- games/carbon/sections/auto_update_validate.sh
+++ games/carbon/sections/auto_update_validate.sh
@@ -42,6 +42,7 @@ if [[ "${DOWNLOAD_METHOD}" == "Depot Downloader" ]]; then
         unzip DepotDownloader.zip -d /home/container
         # Navigate to the DepotDownloader directory
         rm -rf /tmp/*
+        chmod +x /home/container/DepotDownloader
         Info "DepotDownloader installation completed successfully. We need to restart your system in order to complete the install..."
         exit 0
     fi
