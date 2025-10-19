dd11c52daab2ddb70f4c1f9c769cedbfd3e43d64
SturdyStubs <mattl321@hotmail.com>
2025-10-20 00:19:33 +0800

    fixed a syntax error with nested quotes

--- games/carbon/js/sections/replace_startup_variables.js
+++ games/carbon/js/sections/replace_startup_variables.js
@@ -1,5 +1,5 @@
 const { Info, Debug, Success } = require('../helpers/messages');
-const { runQuiet } = require('../utils/shell');
+const { spawnSync } = require('child_process');
 
 function replaceStartupVariables() {
   Debug('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=');
@@ -8,12 +8,17 @@ function replaceStartupVariables() {
   Info('Replacing Startup Variables...');
   // Preserve exact bash semantics used previously:
   // MODIFIED_STARTUP=$(eval echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g')
-  const cmd = "bash -lc 'eval echo "$STARTUP" | sed -e '\''s/{{/${/g'\'' -e '\''s/}}/}/g'\'''";
-  const out = runQuiet(cmd);
-  const modified = (out || '').toString().trim();
+  const bashCmd = "eval echo \"$STARTUP\" | sed -e 's/{{/${/g' -e 's/}}/}/g'";
+  const result = spawnSync('bash', ['-lc', bashCmd], { encoding: 'utf8' });
+  if (result.status !== 0) {
+    const err = result.stderr || (result.error && result.error.message) || 'Failed to expand STARTUP';
+    throw new Error(err);
+  }
+  const modified = (result.stdout || '').trim();
   Debug(`:/home/container$ ${modified}`);
   Success('Variables replaced!');
   return modified;
 }
 
 module.exports = { replaceStartupVariables };
+
